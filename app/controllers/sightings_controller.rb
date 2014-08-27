fef8ce0e4686150ec882dd5f8b79ab788933e369
Carolyn Huynh & Scott Dudley <student@epicodus.com>
2014-08-27 14:42:45 -0700

    adds create and delete for sightings

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,12 +1,21 @@
 class SightingsController < ApplicationController
   def index
     @specie = Species.find(params[:id])
-    @sighting = @specie.sightings.new
+    @sighting = Sighting.new
     render('sightings/index.html.erb')
   end
 
   def create
     @sighting = Sighting.create(params[:sightings])
+    @specie = Species.find(params[:id])
+    @specie.sightings << @sighting
+    render('sightings/index.html.erb')
+  end
+
+  def delete
+    @sighting = Sighting.find(params[:id])
+    @sighting.destroy
+    @specie = Species.find(params[:specie_id])
     render('sightings/index.html.erb')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   match('/species/:id/edit', { :via => :get, :to => 'species#edit' })
   match('/species/:id', { :via => [:patch, :put], :to => 'species#update'})
 
-  match('/species/:id/sightings', { :via => :get, :to => 'sightings#index'})
-  match('/species/:id', { :via => :get, :to => 'sightings#create'})
+  match('/species/:id', { :via => :get, :to => 'sightings#index'})
+  match('/species/:id/sightings', { :via => :post, :to => 'sightings#create'})
+  match('/species/:specie_id/sightings/:id', { :via => :delete, :to => 'sightings#delete'})
 end
