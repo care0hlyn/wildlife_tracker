9eae5ba7c72a0aec6113f55112e448f85993aefc
Carolyn Huynh & Scott Dudley <student@epicodus.com>
2014-08-27 11:00:49 -0700

    Adds delete, update, and edit for species

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -10,4 +10,26 @@ class SpeciesController < ApplicationController
     @specie = Species.create(params[:species])
     render('species/index.html.erb')
   end
+
+  def delete
+    @specie = Species.find(params[:id])
+    @specie.destroy
+    @species = Species.all
+    render('species/index.html.erb')
+  end
+
+  def edit
+    @specie = Species.find(params[:id])
+    render('species/edit.html.erb')
+  end
+
+  def update
+    @species = Species.all
+    @specie = Species.find(params[:id])
+    if @specie.update(params[:species])
+      render('species/index.html.erb')
+    else
+      render('species/edit.html.erb')
+    end
+  end
 end
